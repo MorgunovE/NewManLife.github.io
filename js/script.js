baaa27381635cea113fa8edc96cbe94ed013d32d
Evgenii <Morgunove87@gmail.com>
2020-02-05 09:00:32 +0500

    BAM technoligy

--- js/script.js
+++ js/script.js
@@ -1,17 +1,78 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-        menuItem = document.querySelectorAll('.menu_item'),
-        hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg" alt="right"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+            }
+        }]
+    });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass(' catalog-item__list_active');
+            })
+        });
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+    //modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
         })
-    })
-})
+    });
+
+    function validForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please specify your name",
+                    minlength: jQuery.validator.format("At least {0} characters required!")
+                },
+                phone: "Please type your phone number in the format +12345678900",
+                email: {
+                    required: "We need your email address to contact you",
+                    email: "Your email address must be in the format of name@domain.com"
+                }
+            }
+        });
+    };
+    validForms('#consultationform');
+    validForms('#order form');
+    validForms('#consultation form');
+});
